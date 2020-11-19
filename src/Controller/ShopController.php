0b5bb504d52aae864130c80117be1cc64720558d
Pil <70975556+arnaudpilato@users.noreply.github.com>
2020-11-20 00:51:38 +0100

    fix errors

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -3,11 +3,16 @@
 namespace App\Controller;
 
 use App\Controller\AbstractController;
+use App\Model\ShopManager;
 
 class ShopController extends AbstractController
 {
     public function shop()
     {
-        return $this->twig->render('Shop/shop.html.twig');
+        $shopManager = new ShopManager();
+        $items = $shopManager->selectAll();
+        return $this->twig->render('Shop/shop.html.twig', [
+            'items' => $items
+        ]);
     }
 }
