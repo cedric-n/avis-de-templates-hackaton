95d7bce2903e921aeb8c70057f184288519a95eb
Pil <70975556+arnaudpilato@users.noreply.github.com>
2020-11-20 08:57:10 +0100

    add somethings

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -12,7 +12,21 @@ class ShopController extends AbstractController
         $shopManager = new ShopManager();
         $items = $shopManager->selectAll();
         return $this->twig->render('Shop/shop.html.twig', [
-            'items' => $items
+            'items' => $items,
         ]);
+    }
+
+    public function choice(int $id)
+    {
+        $shopManager = new ShopManager();
+        $select = $shopManager->selectOneById($id);
+        return $this->twig->render('Shop/choice.html.twig', [
+            'item' => $select,
+        ]);
+    }
+
+    public function validate()
+    {
+        return $this->twig->render('Shop/validate.html.twig');
     }
 }
